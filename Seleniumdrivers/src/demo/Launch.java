20f08ba1ecc4f58ad2722facd54105fbd47b3311
HP <HP@DESKTOP-S7R1DIM>
2025-07-01 13:16:25 +0530

    Added launch

---
+++ Seleniumdrivers/src/demo/Launch.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Launch {
+@Test
+public void sample() {
+	WebDriver driver=new ChromeDriver();
+}
+}
